e48b3dde046f96bf2b5d88c90ec6038fb9360c9a
alattari <alattari@csu.fullerton.edu>
2016-06-13 20:06:10 -0300

    Update folhaHuffman.java

--- folhaHuffman.java
+++ folhaHuffman.java
@@ -1,7 +1,7 @@
 package huffmanTeste;
 
 class folhaHuffman extends arvoreHuffman {
-    public final char valor; // valor da folha
+    public final char valor; // valor(caracter) da folha
  
     public folhaHuffman(int freq, char val) {
         super(freq);
